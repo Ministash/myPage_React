91448fde5614cea381b0bec2655939c79269affe
Bob Ross <ministash14@gmail.com>
2019-03-19 18:30:21 -0700

    Making an algoritham for having something fire only when a change is made, but having it only happen once. It is diffuclt.

--- src/components/scrollingPage-Main/scrollingPage.js
+++ src/components/scrollingPage-Main/scrollingPage.js
@@ -5,18 +5,26 @@ class scrollingPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+
             windowHeight: 0,
+            currentBreakingPointNum: 17,
+            lastBreakingPointNum: 17,
+            scrollingBarWidth: "0%",
+            didItFlash: false,
+
             pictures: ["face1", "face3", 'face5', 'face7', 'face9', 'face10', "face11"],
-            objectOpacity: 0,
             currentFace: 'face1',
+
+            objectOpacity: 0,
             transitionOp: 0,
-            scrollingBarWidth: "0%"
 
         };
     }
 
+
     componentDidMount() {
         window.addEventListener('scroll', this.handleScroll);
+        this.animationFunction();
     }
 
 
@@ -28,33 +36,40 @@ class scrollingPage extends React.Component {
         let scrolled = Math.round((winScroll / winHeight) * 100);
 
         this.imageAppend(scrolled);
+        this.animationFunction(scrolled);
 
         this.setState({ scrollingBarWidth: scrolled + "%" });
     }
 
     imageAppend = (scrollNum) => {
 
-        if (scrollNum < 16.67) {
+        if (scrollNum < 17) {
             this.setState({ currentFace: "face1" });
+            this.setState({currentBreakingPointNum: 17});
         }
 
-        else if (scrollNum > 16.67 && scrollNum < 33.34) {
+        else if (scrollNum > 17 && scrollNum < 33) {
             this.setState({ currentFace: "face3" });
+            this.setState({currentBreakingPointNum: 33});
 
         }
-        else if (scrollNum > 33.34 && scrollNum < 50) {
+        else if (scrollNum > 33 && scrollNum < 50) {
             this.setState({ currentFace: "face5" });
+            this.setState({currentBreakingPointNum: 50});
 
         }
-        else if (scrollNum > 50 && scrollNum < 66.67) {
+        else if (scrollNum > 50 && scrollNum < 67) {
             this.setState({ currentFace: "face7" });
+            this.setState({currentBreakingPointNum: 67});
         }
-        else if (scrollNum > 66.67 && scrollNum < 83.67) {
+        else if (scrollNum > 67 && scrollNum < 84) {
             this.setState({ currentFace: "face9" });
+            this.setState({currentBreakingPointNum: 84});
 
         }
-        else if (scrollNum > 83.67 && scrollNum < 99) {
+        else if (scrollNum > 84 && scrollNum < 99) {
             this.setState({ currentFace: "face10" });
+            this.setState({currentBreakingPointNum: 99});
             this.setState({
                 objectOpacity: 0
             });
@@ -69,28 +84,50 @@ class scrollingPage extends React.Component {
         }
     }
 
-    clickFunction = (e) =>{
+    clickFunction = (e) => {
         e.preventDefault();
-        
-        if(this.state.objectOpacity === 1){
+
+        if (this.state.objectOpacity === 1) {
             console.log("I am ready to click!");
-        }else{
-            console.log("Dont ducking click me!");
+        } else {
+
         }
+    }
+
+    animationFunction = (scrollNum) => {
+
+
+        if (scrollNum === this.state.currentBreakingPointNum && this.state.didItFlash === false) {
+            this.setState({ didItFlash: true });
+            this.setState({lastBreakingPointNum: this.state.currentBreakingPointNum});
+        }
+
+        let lastNum = this.state.lastBreakingPointNum;
+        let currentNum = this.state.currentBreakingPointNum;
+        console.log(lastNum);
+        console.log(currentNum);
+
+        // if(lastNum === currentNum){
+        //     this.setState({ didItFlash: false });
+        //    console.log("hello");    
+        // }else{
+        //     console.log("its the same so nothing is going on");
+        // }
     }
 
 
 
     render() {
+
+
         return (
             <div className="encompassing-wrapper">
-                
+
                 <div className="scrolling-page-image-wrapper">
                     <div className="scrolling-page-bar">
                         <div className="scrolling-page-bar-indicator" style={{ width: this.state.scrollingBarWidth }}></div>
                     </div>
                     <div className="scrolling-page-transition-item" style={{ opacity: this.state.transitionOp }}></div>
-
                     {this.state.pictures.map((item, i) => {
                         if (this.state.currentFace === item) {
                             return <div key={i} className={item} style={{ opacity: 1 }} id="scrolling-page-background-main"></div>
